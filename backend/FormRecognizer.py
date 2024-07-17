b754fa0edfda0065efbadad3c5acf119d6c72cde
crunchydosa123 <prathamgadkari@gmail.com>
2024-07-17 17:50:41 +0530

    karra text recognise

--- backend/FormRecognizer.py
+++ backend/FormRecognizer.py
@@ -10,20 +10,14 @@ ENDPOINT = credentials['ENDPOINT']
 
 form_recognizer_client = FormRecognizerClient(ENDPOINT, AzureKeyCredential(API_KEY))
 
-receipt_url = 'https://media-cdn.tripadvisor.com/media/photo-s/13/f7/7a/75/receipt.jpg'
-poller = form_recognizer_client.begin_recognize_receipts_from_url(receipt_url)
+form_url = 'https://raw.githubusercontent.com/ANUJT65/bob_hackathon/main/backend/sample_form.jpg'
+#poller = form_recognizer_client.begin_recognize_receipts_from_url(receipt_url)
+poller = form_recognizer_client.begin_recognize_content_from_url(form_url)
+form_result = poller.result()
 
-time.sleep(3)
-if poller.status() == 'succeeded':
-    result = poller.result()
-    for receipt in result:
-        print(receipt.form_type)
-        for name, field in receipt.fields.items():
-            if name == 'Items':
-                print('Purchase Item')
-                for indx, item in enumerate(field.value):
-                    print('\tItem #{0}'.format(indx + 1))
-                    for item_name, item in item.value.items():
-                        print('\t{0}: {1} Confidence: {2}'.format(item_name, item.value, item.confidence))
-            else:
-                print('{0}: {1} - Confidence {2}'.format(name, field.value, field.confidence))
+for page in form_result:
+    for table in page.tables:
+        for cell in table.cells:
+            print(cell.text)
+    for line in page.lines:
+        print(line.text)
