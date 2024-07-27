bd2e6f59fc2866b4ba0a4393f091d042d1c38280
Suraj Chavan <surajchavan99886@gmail.com>
2024-07-27 17:10:58 +0530

    main buttons added

--- frontend/src/components/DashboardMain.jsx
+++ frontend/src/components/DashboardMain.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import ApplicationCard from './ApplicationCard';
 import NewApplicationForm from './NewApplicationForm';
+import { useNavigate } from 'react-router-dom';
 
 const DashboardMain = () => {
+  const navigate = useNavigate();
+
   const [applications, setApplications] = useState([]);
   const [showNewApplicationForm, setShowNewApplicationForm] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  
 
   useEffect(() => {
     const fetchApplications = async () => {
@@ -48,6 +50,18 @@ const DashboardMain = () => {
           onClick={handleNewApplicationClick}
         >
           New Application
+        </button>
+        <button
+          className='bg-[#ff5b2e] text-white px-5 py-2 mx-4 rounded-md font-bold'
+          onClick={() => navigate('/dashboard')}
+        >
+          New Personal Loan Application
+        </button>
+        <button
+          className='bg-[#ff5b2e] text-white px-5 py-2 mx-4 rounded-md font-bold'
+          onClick={() => navigate('/dashboard')}
+        >
+          New Business Loan Application
         </button>
       </div>
       <h2 className='text-2xl font-bold mb-4'>Your Applications</h2>
