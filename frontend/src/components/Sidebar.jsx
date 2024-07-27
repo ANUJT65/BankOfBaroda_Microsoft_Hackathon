e9490f5f5be77f7bc53b9483a9503560b7cef53d
crunchydosa123 <prathamgadkari@gmail.com>
2024-07-27 08:24:48 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Sidebar = () => {
   return (
-    <div className='flex flex-col h-screen bg-orange-100 w-1/6 p-4'>
-      <div className='p-4 cursor-pointer hover:bg-orange-200 rounded-md'>Applications</div>
-      <div className='p-4 cursor-pointer hover:bg-orange-200 rounded-md'>Grievances</div>
-      <div className='p-4 cursor-pointer hover:bg-orange-200 rounded-md'>Accounts</div>
-      <div className='p-4 cursor-pointer hover:bg-orange-200 rounded-md'>Investments</div>
+    <div className='flex flex-col h-screen bg-white w-1/6 p-4'>
+      <div className='p-4 cursor-pointer hover:bg-black hover:text-white rounded-md'>Applications</div>
+      <div className='p-4 cursor-pointer hover:bg-black hover:text-white rounded-md'>Grievances</div>
+      <div className='p-4 cursor-pointer hover:bg-black hover:text-white rounded-md'>Accounts</div>
+      <div className='p-4 cursor-pointer hover:bg-black hover:text-white rounded-md'>Investments</div>
     </div>
   );
 };
