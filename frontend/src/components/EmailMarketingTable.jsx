332317f135f1d0e3ba8cc0a8be413afc8d6891cd
crunchydosa123 <prathamgadkari@gmail.com>
2024-08-29 15:47:31 +0530

    Update EmailMarketingTable.jsx

--- frontend/src/components/EmailMarketingTable.jsx
+++ frontend/src/components/EmailMarketingTable.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const EmailMarketingTable = () => {
   return (
     <>
-    <div className='text-3xl mt-4 mb-2'>Recipients</div>
+    <div className='text-3xl mt-4 mb-2'>Eligible Customers</div>
     <div className='bg-white py-2 px-3 border border-gray-400 flex justify-start'>
         <input type='checkbox' className=''></input>
         <div className='mx-2 font-bold'>Virat Kohli</div>
