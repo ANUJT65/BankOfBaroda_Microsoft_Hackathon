44852cba01e8a80db1a7ea14a104fd342af992d6
crunchydosa123 <prathamgadkari@gmail.com>
2024-08-29 15:49:35 +0530

    Update EmailMarketingSidebar.jsx

--- frontend/src/components/EmailMarketingSidebar.jsx
+++ frontend/src/components/EmailMarketingSidebar.jsx
@@ -5,9 +5,9 @@ const EmailMarketingSidebar = () => {
     <div className='col-span-2 bg-white flex flex-col p-4'>
         <div className='text-2xl'>Email Campaigns</div>
         <div className='text-gray-500'>Create new campaign</div>
-        <div className='text-gray-500'>Campaign 1</div>
-        <div className='text-gray-500'>Campaign 2</div>
-        <div className='text-gray-500'>Campaign 3</div>
+        <div className='text-gray-500'>Retired Military Personnel Loan</div>
+        <div className='text-gray-500'>Healthcare Loan</div>
+        <div className='text-gray-500'>Elderly Care Loan</div>
     </div>
   )
 }
