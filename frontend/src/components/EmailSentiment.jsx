43b5e6fb08deef020be65f08d9ff65a6bf4b1bcd
crunchydosa123 <prathamgadkari@gmail.com>
2024-08-29 20:15:22 +0530

    sentiment

--- frontend/src/components/EmailSentiment.jsx
+++ frontend/src/components/EmailSentiment.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const EmailSentiment = ({ sentiment }) => {
   let color = '000000';
-  if (sentiment === 'Good') {
-    color = '#72FE04';
-  }  else {
+  if (sentiment === 'negative') {
     color = '#FF0000';
+  }  else {
+    color = '#008000';
   }
 
   return (
     <div className={`text-left flex justify-between py-3`}>
       <div className='font-white text-xl'>Email sentiment</div>
-      <div className={`bg-[${color}] p-2 font-bold`}>{sentiment}</div>
+      <div className={`text-[${color}] p-2 font-bold`}>{sentiment}</div>
     </div>
   );
 }
